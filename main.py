4e19dff0481b12d0db80ca6799633e94b03967c3
abyssian03 <abyssian03@gmail.com>
2024-06-24 06:00:48 +0300

    Add files via upload
    Практическое задание по уроку "Базовые структуры данных"

---
+++ main.py
@@ -0,0 +1,25 @@
+print ("Задача «Длина слова»")
+a = ("мама мыла раму")
+print(len(a))
+print ("Задача «Суммы и разности»")
+first = 5
+second = -2
+summ = (first + second)
+diff = (first - second)
+print(summ, diff)
+print ("Задача «Среднее арифметическое»")
+first = 7
+second = 0
+third = 8
+mean = (first + second + third) / 3
+print (mean)
+print("Задача «Простые строчки»")
+first_string = "Понедельник"
+second_string = "Вторник"
+print(first_string + ', ' + second_string)
+print('Задача «Сложная формула»')
+a = 2
+b = 19
+c = 6
+f = (a * b) + (a * c)
+print ((f ** 3) / 2)

2a5a57d46848209ebc2332b563b63c0517abb912
abyssian03 <abyssian03@gmail.com>
2024-07-08 11:34:27 +0300

    Create module_3_1.py
    Домашняя работа по уроку "Пространство имён"

---
+++ module_3_1.py
@@ -0,0 +1,31 @@
+def count_calls():
+    global calls
+    calls = calls + 1
+
+def string_info(string):
+    count_calls()
+    x = [len(string), string.upper(), string.lower()]
+    return (x)
+
+def is_contains(string, list_to_search):
+    count_calls()
+    if string in list_to_search:
+        return (True)
+    else:
+        return (False)
+
+calls = 0
+choice1 = 'y'
+choice2 = 'y'
+while choice2 == 'y':
+    string = input("Введите слово: ", )
+    print('Результат операций со словом:', string_info(string))
+    choice1 = input('Продолжить? (y/n) ', )
+    if choice1 == 'y':
+        list_to_search = list(input("Введите список слов, разделяя пробелами: ", ).split(" "))
+        print('Проверка наличия слова в списке:', is_contains(string, list_to_search))
+        choice2 = input('Продолжить? (y/n) ', )
+    else:
+        break
+
+print("Количество вызовов функций: ", calls)

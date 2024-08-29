2404e63baa6487a5230d71356f5295383d7b6a1f
abyssian03 <abyssian03@gmail.com>
2024-08-29 20:16:25 +0300

    Update module_9_3.py

--- module_9_3.py
+++ module_9_3.py
@@ -1,6 +1,6 @@
 first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 zipped = list(zip(first, second))
-first_result = [abs(len(pair[0])-len(pair[1])) for pair in zipped if len(pair[0]) != len(pair[1])]
-second_result = [len(first[i]) == len(second[i]) for i in range(len(first))]
-print(first_result, second_result)
+first_result = (abs(len(pair[0])-len(pair[1])) for pair in zipped if len(pair[0]) != len(pair[1]))
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
+print(list(first_result), list(second_result))

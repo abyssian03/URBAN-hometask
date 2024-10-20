68e8546fdf51f687af1c1df3ae6fa021930c3071
abyssian03 <abyssian03@gmail.com>
2024-10-20 22:16:46 +0300

    Update module_11_1.py

--- module_11_1.py
+++ module_11_1.py
@@ -1,5 +1,5 @@
 import requests
 
-r = requests.get('https://github.com/', auth=('abyssian03', 'look4rewardnewcoder'))
+r = requests.get('https://github.com/', auth=('jannynovosib', 'big0blob0bubble'))
 if r.status_code == 200:
     print("OK")
